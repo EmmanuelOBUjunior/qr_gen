bf8c9fe2ee1c03fa0de5b45babb757988c692e20
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-05-04 18:27:51 +0000

    feat:working on the main page

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ export default function Home(){
   const [selectedType, setSelectType] = useState<QRType>('url')
   return(
     <main className="min-h-screen p-8">
-      <div className="max-w-4xl mx-auo">
-        <h1 className="text-4xl font-bold text-center mbb-8">
+      <div className="max-w-4xl mx-auto">
+        <h1 className="text-4xl font-bold text-center mb-8">
           QR Code Generator
         </h1>
       </div>
