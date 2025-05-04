50633d7e2e53d5c910c4d3e970d4fa93c84b1a0c
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-05-04 18:25:24 +0000

    feat:working on the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
+import { useState } from "react"
+
+type QRType = 'pdf'| 'url'|'contact'
 export default function Home(){
+  const [selectedType, setSelectType] = useState<QRType>('url')
   return(
     <div></div>
   )
