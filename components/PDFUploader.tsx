e825b232d07a7e447a492db2dda75a37d9e8c570
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-05-04 18:46:03 +0000

    new:added the contactForm component file

---
+++ components/PDFUploader.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PDFUploader = () => {
+  return (
+    <div>PDFUploader</div>
+  )
+}
+
+export default PDFUploader
---
+++ components/URLInput.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const URLInput = () => {
+  return (
+    <div>URLInput</div>
+  )
+}
+
+export default URLInput
