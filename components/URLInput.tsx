8cf0eff6c1c97ab1d33cf7b6265decfac966c2e3
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-05-04 18:49:57 +0000

    new:added the contactForm component file

--- components/URLInput.tsx
+++ components/URLInput.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import { useState } from "react";
 
 const URLInput = () => {
+    const [url, setUrl] = useState("");
+    const [qrCode, setQrCode] = useState('')
+    const [error, setError] = useState('')
   return (
     <div>URLInput</div>
   )
