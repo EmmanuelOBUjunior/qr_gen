22f90e03fef00dda98d1341f5a7a06c178649dcb
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-05-04 18:27:09 +0000

    feat:working on the main page

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,12 @@ type QRType = 'pdf'| 'url'|'contact'
 export default function Home(){
   const [selectedType, setSelectType] = useState<QRType>('url')
   return(
-    <main className="miin-h-screen p-8">
-
+    <main className="min-h-screen p-8">
+      <div className="max-w-4xl mx-auo">
+        <h1 className="text-4xl font-bold text-center mbb-8">
+          QR Code Generator
+        </h1>
+      </div>
     </main>
   )
 }
