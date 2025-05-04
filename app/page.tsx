c3a4c300c39b3f6e4c81178e0e2b358f9f7f2770
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-05-04 18:40:28 +0000

    feat:working on the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,44 @@
-'use client'
-import { useState } from "react"
+"use client";
+import { useState } from "react";
 
-type QRType = 'pdf'| 'url'|'contact'
-export default function Home(){
-  const [selectedType, setSelectType] = useState<QRType>('url')
-  return(
+type QRType = "pdf" | "url" | "contact";
+export default function Home() {
+  const [selectedType, setSelectedType] = useState<QRType>("url");
+  return (
     <main className="min-h-screen p-8">
       <div className="max-w-4xl mx-auto">
         <h1 className="text-4xl font-bold text-center mb-8">
           QR Code Generator
         </h1>
         <div className="flex justify-center gap-4 mb-8">
-          <button onClick={()=>setSelectedType('pdf')} className={`px-4 py-2 rounded ${selectedType === 'url'? 'bg-blue-500 text-white': 'bg-gray-200'}`}>URL</button>
-          <button onClick={()=>setSelectedType('pdf')} className={`px-4 py-2 rounded ${selectedType === 'pdf'? 'bg-blue-500 text-white': 'g-gray-200'}`}>PDF</button>
-          <button className={`px-4 py-2 rounded ${selectedType === 'contact'? 'bg-blue-500 text-white': 'bg-gray-200'}`}>Contact</button>
+          <button
+            onClick={() => setSelectedType("pdf")}
+            className={`px-4 py-2 rounded ${
+              selectedType === "url" ? "bg-blue-500 text-white" : "bg-gray-200"
+            }`}
+          >
+            URL
+          </button>
+          <button
+            onClick={() => setSelectedType("pdf")}
+            className={`px-4 py-2 rounded ${
+              selectedType === "pdf" ? "bg-blue-500 text-white" : "g-gray-200"
+            }`}
+          >
+            PDF
+          </button>
+          <button
+          onClick={()=> setSelectedType("contact")}
+            className={`px-4 py-2 rounded ${
+              selectedType === "contact"
+                ? "bg-blue-500 text-white"
+                : "bg-gray-200"
+            }`}
+          >
+            Contact
+          </button>
         </div>
       </div>
     </main>
-  )
+  );
 }
