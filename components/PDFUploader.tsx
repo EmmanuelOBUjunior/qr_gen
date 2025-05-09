d3f3099a94ff2d7fa34bee4352e43d5ecd5906ab
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-05-09 15:47:11 +0000

    feat:working on the contact form

--- components/PDFUploader.tsx
+++ components/PDFUploader.tsx
@@ -2,6 +2,16 @@ import {useState} from 'react'
 import  QRCode  from 'qrcode'
 
 const PDFUploader = () => {
+  const [file, setFile] = useState<File | null>(null)
+  const [uploading, setUploading] = useState(false)
+  const [error, setError] = useState<string | null>(null)
+  const [qrCode, setQrCode] = useState<string | null>(null)
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if(e.target.files && e.target.files[0]){
+      setFile(e.target.files[0])
+    }
+  }
   return (
     <div>PDFUploader</div>
   )
