a8351d3acdcd04ce3a375be6bbaaf37ba2683d06
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-05-09 09:14:53 +0000

    feat:working on the contact form

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import {useState} from 'react'
+import QRCode from 'qrcode'
+
+interface ContactInfo{
+  name:string;
+  phone: string;
+  email: string;
+  organization?: string;
+}
 
 const ContactForm = () => {
+  const [contactInfo, setContactInfo] = useState<ContactInfo>({
+    name: '',
+    phone: '',
+    email: '',
+    organization: ''
+  })
   return (
     <div>ContactForm</div>
   )
