91c2725f63c92bdb956317df7988af2a0df34a3f
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-05-09 15:23:11 +0000

    feat:working on the contact form

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -17,12 +17,13 @@ const ContactForm = () => {
     organization: "",
   });
   const [qrCode, setQrCode] = useState<string>("");
-  const generateVCard = async (contact: ContactInfo) => {
-    return `BEGIN:VCARD VERSION:3.0 FN:${contact.name} TEL:${
-      contact.phone
-    } EMAIL:${contact.email} ${
-      contact.organization ? `ORG:${contact.organization}\n` : ""
-    }  END:VCARD`;
+  const generateVCard = (contact: ContactInfo) => {
+    return `BEGIN:VCARD
+VERSION:3.0
+FN:${contact.name}
+TEL:${contact.phone}
+EMAIL:${contact.email}
+${contact.organization ? `ORG:${contact.organization}\n` : ''}END:VCARD`;
   };
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -88,23 +89,42 @@ const ContactForm = () => {
           />
         </div>
         <div>
-          <label htmlFor="organization" className="block text-sm font-medium text-gray-700">Organization (Optional)</label>
+          <label
+            htmlFor="organization"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Organization (Optional)
+          </label>
           <input
             type="text"
             id="organization"
             className="p-2 mt-1 block w-full rounded border border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
             value={contactInfo.organization}
             onChange={(e) =>
-              setContactInfo({...contactInfo, organization: e.target.value })
+              setContactInfo({ ...contactInfo, organization: e.target.value })
             }
           />
         </div>
-        <button className="mt-2 w-full bg-blue-500 rounded text-white py-2 px-4 hover:bg-blue-600">Generate QR Code</button>
+        <button className="mt-2 w-full bg-blue-500 rounded text-white py-2 px-4 hover:bg-blue-600">
+          Generate QR Code
+        </button>
 
         {qrCode && (
-          <div className='text-center'>
-            <Image width={250} height={250} src={qrCode} alt="QR Code" className="mx-auto"/>
-            <a className="mt-2 inline-block text-blue-500 hover:text-blue-600" download='contact-qr-code.png' href={qrCode}>Download QR Code</a>
+          <div className="text-center">
+            <Image
+              width={250}
+              height={250}
+              src={qrCode}
+              alt="QR Code"
+              className="mx-auto"
+            />
+            <a
+              className="mt-2 inline-block text-blue-500 hover:text-blue-600"
+              download="contact-qr-code.png"
+              href={qrCode}
+            >
+              Download QR Code
+            </a>
           </div>
         )}
       </div>
