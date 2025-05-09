a161ef6f5ef8858e92d3f6f005ba17a3a8d6c0cd
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2025-05-09 07:58:27 +0000

    feat:working on urlInput component ui

--- components/URLInput.tsx
+++ components/URLInput.tsx
@@ -1,33 +1,42 @@
 import { useState } from "react";
-import QRCode from 'qrcode'
+import QRCode from "qrcode";
 
 const URLInput = () => {
-    const [url, setUrl] = useState("");
-    const [qrCode, setQrCode] = useState('')
-    const [error, setError] = useState('')
+  const [url, setUrl] = useState("");
+  const [qrCode, setQrCode] = useState("");
+  const [error, setError] = useState("");
 
-    const generateQRCode = async()=>{
-      try{
-        if(!url){
-          setError('Please enter a URL')
-          return
-        }
-        const qrDataURL = await QRCode.toDataURL(url);
-        setQrCode(qrDataURL)
-        setError('')
-      }catch(error){
-        setError('Failed to generate QR code. Please try again later')
-        console.error(error)
+  const generateQRCode = async () => {
+    try {
+      if (!url) {
+        setError("Please enter a URL");
+        return;
       }
+      const qrDataURL = await QRCode.toDataURL(url);
+      setQrCode(qrDataURL);
+      setError("");
+    } catch (error) {
+      setError("Failed to generate QR code. Please try again later");
+      console.error(error);
     }
+  };
   return (
     <div className="space-y-4">
       <div>
-        <label htmlFor="url" className="block text-sm font-medium text-gray-700">Enter URL</label>
-        <input type="url" id="url" className="mt-1 block w-full rounded-md bborder-gray-500 shadow-sm focus:border-blue-500 focus:ring-blue-500"/>
+        <label
+          htmlFor="url"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Enter URL
+        </label>
+        <input
+          type="url"
+          id="url"
+          className="mt-1 block w-full rounded-md bborder-gray-500 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default URLInput
+export default URLInput;
